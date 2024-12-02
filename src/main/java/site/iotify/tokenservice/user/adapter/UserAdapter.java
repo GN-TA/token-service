1e7fa3c550544ef6f7156ad40b0415bec561b2ca
qaw <osshun21@gmail.com>
2024-12-03 00:26:20 +0900

    fix: 자잘한 변수명 수정 및 jwt에 권한 정보 추가 cicd 추가

--- src/main/java/site/iotify/tokenservice/user/adapter/UserAdapter.java
+++ src/main/java/site/iotify/tokenservice/user/adapter/UserAdapter.java
@@ -1,24 +1,33 @@
 package site.iotify.tokenservice.user.adapter;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import site.iotify.tokenservice.user.dto.UserInfo;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class UserAdapter {
-    private final String USER_API_URL = "http://localhost:8090";
     private final RestTemplate restTemplate;
+
+    @Value("${user.host}")
+    private String host;
+
+    @Value("${user.port}")
+    private int port;
 
     public UserInfo getUserInfo(String emailId) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String> request = new HttpEntity<>(headers);
+        String url = String.format("http://%s:%d/user?email=%s", host, port, emailId);
 
         ResponseEntity<UserInfo> response = restTemplate.exchange(
-                USER_API_URL + "/user?email=" + emailId,
+                url,
                 HttpMethod.GET,
                 request,
                 UserInfo.class
@@ -32,8 +41,10 @@ public class UserAdapter {
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String> request = new HttpEntity<>(headers);
 
+        String url = String.format("http://%s:%d/pwd?id=%s", host, port, email);
+
         ResponseEntity<String> response = restTemplate.exchange(
-                USER_API_URL + "/pwd?id=" + email,
+                url,
                 HttpMethod.GET,
                 request,
                 String.class
